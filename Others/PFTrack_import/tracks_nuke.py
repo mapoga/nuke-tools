03189a2c6837c3a4b55e5f8bc5805a27460059f3
mapoga <mathieugouletaubin@gmail.com>
2018-04-03 18:03:01 -0400

    wip cleaning

--- Others/PFTrack_import/tracks_nuke.py
+++ Others/PFTrack_import/tracks_nuke.py
@@ -1,49 +1,50 @@
+"""
+Creates a tracker3 or tracker4 node from a list of tracker keys.
+trackers = [
+    {'name': 'TrackerName',
+     'keys': [
+        {'frame': 0,
+         'x': 10,
+         'y': 20},
+     ]
+    },
+]
+
+"""
 import nuke
 
 
-def _chunks(lst, length):
-    # Yield successive n-sized chunks from lst
-    for i in xrange(0, len(lst), length):
-        yield lst[i:i + length]
-
 
 def create_tracker3(trackers, label=''):
+
+    def iter_chunk(lst, size):
+        """Yield successive n-sized chunks from lst"""
+        for i in xrange(0, len(lst), size):
+            yield lst[i:i + size]
+
     nodes = []
     # Chunk tracks by 4
-    for chunks in _chunks(trackers, 4):
-
+    for chunks in iter_chunk(trackers, 4):
         # Create node
         node = nuke.nodes.Tracker3()
         nodes.append(node)
         if label:
             node['label'].setValue(label)
             node['enable1'].setFlag(0)
-
-        # tracks loop
-        for idx, track in enumerate(chunks):
-            kid = idx+1
-            node['enable{0}'.format(kid)].setValue('true')
-            knob = node['track{0}'.format(kid)]
+        for idx, track in enumerate(chunks, 1):
+            # knobs
+            node['enable{0}'.format(idx)].setValue('true')
+            knob = node['track{0}'.format(idx)]
             knob.setAnimated()
-            keys_x = []
-            keys_y = []
-
-            for key in track['keys']:
-                frame = int(key['frame'])
-                x = float(key['x'])
-                y = float(key['y'])
-
-                keys_x.append((frame, x))
-                keys_y.append((frame, y))
-
             anim_x = knob.animation(0)
-            anim_x.addKey([nuke.AnimationKey(frame, value)
-                           for (frame, value) in keys_x])
-
             anim_y = knob.animation(1)
-            anim_y.addKey([nuke.AnimationKey(frame, value)
-                           for (frame, value) in keys_y])
-
+            # animation
+            keys_x = [nuke.AnimationKey(key['frame'], key['x'])
+                      for key in track['keys']]
+            keys_y = [nuke.AnimationKey(key['frame'], key['y'])
+                      for key in track['keys']]
+            anim_x.addKey(keys_x)
+            anim_y.addKey(keys_y)
     return nodes
 
 
@@ -54,8 +55,7 @@ def create_tracker4(trackers, label=''):
     # http://communiy.foundry.com/discuss/topic/99665
     # /python-with-new-tracker?mode=Post&postID=969555
 
-    # Slower to create than tracker3 because of
-    # lack of animation function
+    # Slower to create than tracker3, lacking animation function
 
     # Create node
     node = nuke.createNode('Tracker4')
