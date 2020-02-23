f7871ecfa10c747e1d865f53ed4e1b71e0d21cf8
mapoga <mathieugouletaubin@gmail.com>
2020-02-22 20:44:31 -0500

    LensDefects

--- init.py
+++ init.py
@@ -1,23 +1,15 @@
-import os.path
-
-
-github = r'C:/Users/Mapoga/Documents/GitHub/'
-#######################################################
-##################### Tools Begin #####################
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/3D'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/3D/icons'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Edge'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Edge/icons'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/icons'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Time'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Time/icons'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Views'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Views/icons'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Others'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Others/icons'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Others/mochaAE_import'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Others/mmColorTarget'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Others/dg_PerspLines'))
-nuke.pluginAddPath(os.path.join(github, r'nuke-tools/Others/PFTrack_import'))
-##################### Tools Begin #####################
-#######################################################
+nuke.pluginAddPath(r'3D')
+nuke.pluginAddPath(r'3D/icons')
+nuke.pluginAddPath(r'Edge')
+nuke.pluginAddPath(r'Edge/icons')
+nuke.pluginAddPath(r'icons')
+nuke.pluginAddPath(r'Time')
+nuke.pluginAddPath(r'Time/icons')
+nuke.pluginAddPath(r'Views')
+nuke.pluginAddPath(r'Views/icons')
+nuke.pluginAddPath(r'Others')
+nuke.pluginAddPath(r'Others/icons')
+nuke.pluginAddPath(r'Others/mochaAE_import')
+nuke.pluginAddPath(r'Others/mmColorTarget')
+nuke.pluginAddPath(r'Others/dg_PerspLines')
+nuke.pluginAddPath(r'Others/PFTrack_import')
